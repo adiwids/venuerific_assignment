8da06fd4d255f5c8f6fd0d32596aec04111cf512
Adi Widyawan <adi.widyawan@bocistudio.com>
2021-05-22 23:18:44 +0700

    Apply SB Clean Blog template as blog mock-up.

--- app/javascript/packs/admin/base.js
+++ app/javascript/packs/admin/base.js
@@ -1,12 +1,14 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
 Rails.start()
 Turbolinks.start()
-ActiveStorage.start()
 
 require("trix")
 require("@rails/actiontext")
-require("startbootstrap-clean-blog/dist/js/scripts")
+
+require("@popperjs/core/dist/cjs/popper")
+require("jquery/src/jquery")
+require("bootstrap/dist/js/bootstrap")
+
+import "@fortawesome/fontawesome-free/js/all"
