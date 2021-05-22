a4e38baa90ef6c631b5cde0b48eeff81a2a50dd1
Adi Widyawan <adi.widyawan@bocistudio.com>
2021-05-22 11:08:30 +0700

    Implement user registration, sign in and sign out using Devise.

---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  role                   :string           default("regular_user")
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+class User < ApplicationRecord
+  ROLES = {
+    regular_user: 'regular_user',
+    administrator: 'administrator'
+  }.freeze
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :trackable
+
+  enum role: ROLES
+
+  before_validation :ensure_role, on: :create
+
+  private
+
+  def ensure_role
+    self.role = ROLES[:regular_user] unless role
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: %i[confirmation unlock]
+  root to: 'blog/home#index'
+  devise_scope :user do
+    scope module: 'admin' do
+      get '/admin', to: 'dashboard#index', as: 'admin_root'
+    end
+  end
 end
